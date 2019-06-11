6559d994f047c454347ff6932c67a8318853380a
fiona kotzurek <fiona.kotzurek@gmail.com>
2019-06-11 14:15:45 +0200

    pages controller - skip before action :fiona

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :peter]
+  skip_before_action :authenticate_user!, only: [:home, :peter, :fiona]
 
   def home
   end
